a58e6f5f1cd1459798f079ecca580b5d1f201066
LaQuentin <lajusqu@gmail.com>
2022-10-13 16:56:16 +0200

    création component ami

---
+++ src/app/amis/amis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amis',
+  templateUrl: './amis.component.html',
+  styleUrls: ['./amis.component.css']
+})
+export class AmisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
