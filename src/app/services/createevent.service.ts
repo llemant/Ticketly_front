17dd99c2901c49d47095c42d734e3d342aae4a0c
LaQuentin <lajusqu@gmail.com>
2022-10-18 14:09:49 +0200

    Ajouts evenment et lieu + 2 services

---
+++ src/app/services/createevent.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreateeventService {
+  msgErr: any;
+  msgOK: any;
+  constructor() { }
+}
---
+++ src/app/services/createlieu.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreatelieuService {
+  msgErr: any;
+  msgOK: any;
+  constructor() { }
+}
