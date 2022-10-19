02dcb84b7f64f3592e8220a7609ff25c536c04f1
LiseDR <lisedefo.dev@gmail.com>
2022-10-19 16:59:41 +0200

    Boutique en cours

--- src/app/boutique/boutique.component.ts
+++ src/app/boutique/boutique.component.ts
@@ -1,5 +1,7 @@
+import { validateHorizontalPosition } from '@angular/cdk/overlay';
 import { HttpClient } from '@angular/common/http';
 import { Component, Host, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { HostService } from '../services/host.service';
 
@@ -11,6 +13,8 @@ import { HostService } from '../services/host.service';
 export class BoutiqueComponent implements OnInit {
   avantage: any;
   avantages: any;
+  val: any;
+  quantity = new FormControl();
 
   constructor(private http: HttpClient, public authService: AuthService, public host: HostService) {
   }
@@ -21,7 +25,21 @@ export class BoutiqueComponent implements OnInit {
 
   paiement(prix: any) {
     this.http.patch(this.host.myDevHost + 'points/pay/' + prix + '/' + this.authService.getUserSession().id, '').subscribe();
+    console.log(this.quantity.value);
     console.log('ok paiement');
+  }
+
+  obtenir(id: any, prix: any) {
+    this.paiement(prix);
+    this.achat(id);
+  }
+
+  achat(val: any) {
+    val.user.id = this.authService.getUserSession().id;
+    val.avantage.id = val;
+    val.quantite = this.quantity.value;
+    console.log(val);
+    this.http.post(this.host.myDevHost + 'achat-bonus', val).subscribe();
   }
 
   recupAvantages() {
