dfc61c1e31d4fffc46ab97217aa8f3f15e53f96e
AhadiMahaboubi <ahadimahaboubi@gmail.com>
2022-10-20 17:47:23 +0200

    Début événement, allez je vais rntrer moi mdr

--- src/app/mesevenements/mesevenements.component.ts
+++ src/app/mesevenements/mesevenements.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { HostService } from '../services/host.service';
 
 @Component({
   selector: 'app-mesevenements',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MesevenementsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AuthService : AuthService, private host : HostService, private http : HttpClient) { }
 
   ngOnInit(): void {
+    this.recupEventOrganisateurs();
+  }
+  eventsOrgansiateurs : any;
+
+  recupEventOrganisateurs() {
+    this.http.get(this.host.myDevHost + 'eventorga/'+ this.AuthService.getUserSession().id).subscribe({
+      /* next : (data) => { this.eventsOrgansiateurs = data }, */
+      next : (data) => { this.eventsOrgansiateurs = data },
+      error : (err) => { console.log(err) }
+    });
   }
 
 }
