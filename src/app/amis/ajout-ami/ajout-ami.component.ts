4911ae4a9f4aaacc841cf0e4f4c251dc65c8ab56
mcaroo <caroline7777@hotmail.fr>
2022-10-21 12:15:05 +0200

    ajout amis en cours

---
+++ src/app/amis/ajout-ami/ajout-ami.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-ami',
+  templateUrl: './ajout-ami.component.html',
+  styleUrls: ['./ajout-ami.component.css']
+})
+export class AjoutAmiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ajoutAmi(login: any){
+    
+  }
+
+}
