4e9c3c94b3e042f393396352beae35f3f8f87809
mcaroo <caroline7777@hotmail.fr>
2022-10-21 13:01:11 +0200

    demande amis ok

--- src/app/amis/ajout-ami/ajout-ami.component.ts
+++ src/app/amis/ajout-ami/ajout-ami.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from 'src/app/services/auth.service';
+import { HostService } from 'src/app/services/host.service';
+import { MsgInfoComponent } from '../msg-info/msg-info.component';
 
 @Component({
   selector: 'app-ajout-ami',
@@ -6,14 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ajout-ami.component.css']
 })
 export class AjoutAmiComponent implements OnInit {
-
-  constructor() { }
+  durationInSeconds = 4;
+  demande: any;
+  constructor(private http: HttpClient, private host: HostService, public authService: AuthService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
-  ajoutAmi(login: any){
-    
+  ajoutAmi(log: any) {
+    let demande = { demandeur: this.authService.getUserSession(), receveur: { login: log } };
+    this.http.post(this.host.myDevHost + 'amis', demande).subscribe({
+      next: (data) => { this.demande = data; if (this.demande != null) { this.authService.demande = true; } else { this.authService.demande = false; } this.openSnackBar() },
+      error: (err) => { console.log(err); }
+    });
+  }
+
+  openSnackBar() {
+    this._snackBar.openFromComponent(MsgInfoComponent, {
+      duration: this.durationInSeconds * 1000,
+    });
   }
 
 }
