853f98f19fe9824254d8398611c2f6fadd5e7f9e
llemant <louislemant@yahoo.fr>
2022-11-03 17:45:42 +0100

    image -> photo dans creation event, changement image pts fidel sur dashboard, requiered sur form signin user et unicité mail/login à l'inscription user

--- src/app/creation-event/creation-event.component.ts
+++ src/app/creation-event/creation-event.component.ts
@@ -8,34 +8,36 @@ import { HostService } from '../services/host.service';
 @Component({
   selector: 'app-creation-event',
   templateUrl: './creation-event.component.html',
-  styleUrls: ['./creation-event.component.css']
+  styleUrls: ['./creation-event.component.css'],
 })
 export class CreationEventComponent implements OnInit {
   msg: any;
 
+  regexGenre = new RegExp('^[a-zA-Z]');
+  regexPrix = new RegExp('^[0-9]*[1-9][0-9]*$');
+  regexPhoto = new RegExp(
+    '^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$'
+  );
 
-
-  regexGenre = new RegExp("^[a-zA-Z]");
-  regexPrix = new RegExp("^[0-9]*[1-9][0-9]*$");
-  regexPhoto = new RegExp("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$");
-
-  msgAttributManquant = "Un attribut est manquant";
-  msgGenreIncorrect = "Le format du genre est incorrect";
+  msgAttributManquant = 'Un attribut est manquant';
+  msgGenreIncorrect = 'Le format du genre est incorrect';
   msgPrixIncorrect = "Le format du prix est incorrect : c'est un entier";
   msgPhotoIncorrect = "Le format de l'image est incorrect : c'est un lien";
 
+  constructor(
+    public creationEvent: CreateeventService,
+    public authService: AuthService,
+    private http: HttpClient,
+    private route: Router,
+    private host: HostService
+  ) {}
 
-
-  constructor(public creationEvent: CreateeventService, public authService: AuthService, private http: HttpClient, private route: Router, private host: HostService) { }
-
-  ngOnInit(): void { 
-    if(!this.authService.isConnected()){
+  ngOnInit(): void {
+    if (!this.authService.isConnected()) {
       this.route.navigateByUrl('login');
-      this.authService.msgErr = "Veuillez vous connecter";
+      this.authService.msgErr = 'Veuillez vous connecter';
     }
   }
-
-
 
   creerEvent(event: any) {
     event.organisateur = this.authService.getUserSession();
@@ -46,35 +48,30 @@ export class CreationEventComponent implements OnInit {
     if (!this.regexPrix.test(event.prix)) {
       this.creationEvent.msgErr = this.msgPrixIncorrect;
     }
-    if (!this.regexPhoto.test(event.photo)) {
+    if (!this.regexPhoto.test(event.image)) {
       this.creationEvent.msgErr = this.msgPhotoIncorrect;
     }
 
-
-
-    if (this.regexGenre.test(event.genre) &&
+    if (
+      this.regexGenre.test(event.genre) &&
       this.regexPrix.test(event.prix) &&
-      this.regexPhoto.test(event.photo)
+      this.regexPhoto.test(event.image)
     ) {
-      if(this.authService.getUserSession().nbToken < 200) {
-        this.creationEvent.msgErr = "Merci d'acheter des tokens pour créer votre événement, la création d'un événement vous coutera 200 tokens" 
+      if (this.authService.getUserSession().nbToken < 200) {
+        this.creationEvent.msgErr =
+          "Merci d'acheter des tokens pour créer votre événement, la création d'un événement vous coutera 200 tokens";
       } else {
+        this.http.post(this.host.myDevHost + 'event', event).subscribe({
+          next: (data) => {
+            event = data;
+            this.authService.setUserInSession(event.organisateur);
 
-      
-
-      this.http.post(this.host.myDevHost + 'event', event).subscribe({
-        next: (data) => {
-          event = data;
-          this.authService.setUserInSession(event.organisateur)
-
-          this.creationEvent.msgErr = "";
-          this.creationEvent.msgOK = "";
-          this.route.navigateByUrl('profile');
-        },
-
-      })
+            this.creationEvent.msgErr = '';
+            this.creationEvent.msgOK = '';
+            this.route.navigateByUrl('profile');
+          },
+        });
+      }
     }
-
   }
-}
 }
